e827c471d11ae40ea91e620bacad3d1c0b73fc2f
alinhi75 <sayedali.noohi@studenti.polito.it>
2024-09-07 14:31:04 +0200

    Create_Migration_Database_

--- job-recommendation-system/database/migrations/0001_01_01_000000_create_users_table.php
+++ job-recommendation-system/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('Job Seeker');
+            $table->string('resume')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
